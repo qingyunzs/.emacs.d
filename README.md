c832aae5477648234a16bdbb37be1834580c73e0
zhaorengui <zrg1390556487@gmail.com>
2019-01-15 09:59:13 +0800

    install plantuml-mode package

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # .emacs.d
 my directory of .emacs.d
+
+> Emacs 的使用请参考文章：GNU Emacs
--- init.el
+++ init.el
@@ -40,14 +40,15 @@
       (expand-file-name "~/.emacs.d/plantuml/plantuml.jar"))
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(package-selected-packages (quote (htmlize org))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (plantuml-mode htmlize org)))
+ '(plantuml-jar-path "/home/zrg/.emacs.d/plantuml/plantuml.jar"))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
