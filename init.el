c85a73ee0781f8e426b5438940f733591820f546
zhaorengui <zrg1390556487@gmail.com>
2019-09-28 23:17:51 +0800

    update config.

--- init.el
+++ init.el
@@ -40,7 +40,7 @@
     (ruby . t)
     (sass . t)
     (scheme . t)
-    (sh . t)
+    (shell . t)
     (sql . t)
     (sqlite . t)
     ))
@@ -48,9 +48,16 @@
 (add-to-list 'load-path "~/.emacs.d/lisp")
 (require 'init-org-jekyll)
 (require 'emmet-mode)
+(require 'wheatgrass-theme)
+(require 'org-tempo)
 ;; Load plantuml
 (setq org-plantuml-jar-path
       (expand-file-name "~/.emacs.d/plantuml/plantuml.jar"))
+;; company mode
+(global-company-mode t)
+;; Show line numbers
+;;(global-linum-mode t);
+;;(setq linum-format "%d ")
 ;; Emmet optional settings
 ;; You probably want to add it to auto-load on your sgml modes:
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
@@ -78,13 +85,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (monokai-pro)))
+ '(ansi-color-names-vector
+   ["#2d2a2e" "#ff6188" "#a9dc76" "#ffd866" "#78dce8" "#ab9df2" "#ff6188" "#fcfcfa"])
+ '(ansi-term-color-vector
+   [unspecified "#2d2a2e" "#ff6188" "#a9dc76" "#ffd866" "#78dce8" "#ab9df2" "#ff6188" "#fcfcfa"])
+ '(custom-enabled-themes (quote (wheatgrass)))
  '(custom-safe-themes
    (quote
     ("3a2803566972e3f3882af48ce5680b824e81bf9dc5f32639b5f1e6d9f6aeda3c" "1d2f406a342499f0098f9388b87d05ec9b28ccb12ca548f4f5fa80ae368235b6" default)))
  '(package-selected-packages
    (quote
-    (highlight-indentation org-table-comment org plantuml-mode monokai-pro-theme org-plus-contrib emmet-mode smex web-mode company-c-headers company-go company-shell company-php htmlize)))
+    (org-drill-table markdownfmt markdown-mode+ orgalist org-mind-map markdown-mode highlight-indentation org plantuml-mode monokai-pro-theme org-plus-contrib emmet-mode smex web-mode company-c-headers company-go company-shell company-php htmlize)))
  '(plantuml-jar-path "~/.emacs.d/plantuml/plantuml.jar"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
