414abd961863b6e9d66b8d20007415a3645a94d2
zrg <zrg1390556487@gmail.com>
2020-01-01 10:51:19 +0800

    add htmlize.el;update init.el

--- init.el
+++ init.el
@@ -49,6 +49,7 @@
 (require 'init-org-jekyll)
 (require 'emmet-mode)
 (require 'wheatgrass-theme)
+(require 'htmlize)
 ;; Load plantuml
 (setq org-plantuml-jar-path
       (expand-file-name "~/.emacs.d/plantuml/plantuml.jar"))
@@ -98,7 +99,7 @@
     ("3a2803566972e3f3882af48ce5680b824e81bf9dc5f32639b5f1e6d9f6aeda3c" "1d2f406a342499f0098f9388b87d05ec9b28ccb12ca548f4f5fa80ae368235b6" default)))
  '(package-selected-packages
    (quote
-    (company org-drill-table markdownfmt markdown-mode+ orgalist org-mind-map markdown-mode highlight-indentation org plantuml-mode monokai-pro-theme org-plus-contrib emmet-mode smex web-mode company-c-headers company-go company-shell company-php htmlize)))
+    (org-download company org-drill-table markdownfmt markdown-mode+ orgalist org-mind-map markdown-mode highlight-indentation org plantuml-mode monokai-pro-theme org-plus-contrib emmet-mode smex web-mode company-c-headers company-go company-shell company-php htmlize)))
  '(plantuml-jar-path "~/.emacs.d/plantuml/plantuml.jar"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
